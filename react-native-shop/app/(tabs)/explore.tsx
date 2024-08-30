1aa5e850304b62d349ad1192e9ea547fe9b6c69c
SimeonNedyalkov <112097158+SimeonNedyalkov@users.noreply.github.com>
2024-08-30 11:11:20 +0300

    app created

---
+++ react-native-shop/app/(tabs)/explore.tsx
@@ -0,0 +1,20 @@
+import { Image, StyleSheet, Platform,View, Text } from 'react-native';
+
+export default function TabTwoScreen() {
+  return (
+    <View style={styles.titleContainer}>
+      <Text style={{color:'red'}}>Hello</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignContent:'center',
+    justifyContent:'center',
+    gap: 8,
+    flex:1,
+  },
+});
