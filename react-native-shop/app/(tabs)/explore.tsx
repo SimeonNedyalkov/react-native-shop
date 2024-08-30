7d6907d36d5cd2432e67ed2e3220a466184bf4a0
SimeonNedyalkov <112097158+SimeonNedyalkov@users.noreply.github.com>
2024-08-30 12:10:07 +0300

    home view made

--- react-native-shop/app/(tabs)/explore.tsx
+++ react-native-shop/app/(tabs)/explore.tsx
@@ -1,20 +1,41 @@
-import { Image, StyleSheet, Platform,View, Text } from 'react-native';
-
-export default function TabTwoScreen() {
+import { Image, StyleSheet, Platform,View, Text,ImageBackground } from 'react-native';
+export default function HomeScreen() {
   return (
     <View style={styles.titleContainer}>
-      <Text style={{color:'red'}}>Hello</Text>
+      <View style={{flex:2,flexDirection:'row',paddingTop:Platform.OS ==='android'?40:0,backgroundColor:'black',width:'100%'}}>
+        <View style={{backgroundColor:'orange',width:100,height:100}}></View>
+        <View style={{width:100,height:100,backgroundColor:'green'}}></View>
+      </View>
+      
+      <View style={{flex:8}}>
+        <Image source={require('../../assets/images/white-chair-room_786255-18256.jpg')} style={styles.image}>
+
+        </Image>
+      </View>
+      <View style={{flex:2,backgroundColor:'black',width:'100%'}}>
+          
+        </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    alignContent:'center',
-    justifyContent:'center',
-    gap: 8,
     flex:1,
+  },
+  image: {
+    flex:1,
+    width:'100%',
+    height:'100%',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'black',
+    fontSize: 24,
+    lineHeight: 50,
+    fontWeight: 'bold',
+    bottom:120,
+    textAlign: 'center',
+    
   },
 });
--- react-native-shop/app/(tabs)/index.tsx
+++ react-native-shop/app/(tabs)/index.tsx
@@ -1,20 +1,46 @@
-import { Image, StyleSheet, Platform,View, Text } from 'react-native';
-
+import { Image, StyleSheet, Platform,View, Text,ImageBackground } from 'react-native';
 export default function HomeScreen() {
   return (
     <View style={styles.titleContainer}>
-      <Text style={{color:'red'}}>Hello</Text>
+      <View style={{flex:10}}>
+        <ImageBackground source={require('../../assets/images/pexels-photo-5894614.webp')} resizeMode="cover" style={styles.image}>
+          <Image source={require('../../assets/images/R.png')} style={styles.logo}/>
+          <Text style={styles.text}>Sell What You Don't Need</Text>
+        </ImageBackground>
+      </View>
+      <View style={{flex:1,backgroundColor:'orange',width:'100%'}}>
+        <Text>Hello</Text>
+      </View>
+      <View style={{flex:1,backgroundColor:'green',width:'100%'}}>
+        <Text>Hello</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    alignContent:'center',
-    justifyContent:'center',
-    gap: 8,
     flex:1,
   },
+  image: {
+    flex:1,
+    width:'100%',
+    height:'100%',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'black',
+    fontSize: 24,
+    lineHeight: 50,
+    fontWeight: 'bold',
+    bottom:120,
+    textAlign: 'center',
+    
+  },
+  logo:{
+    width:100,
+    height:100,
+    bottom:150,
+    left:150
+  }
 });
