0bd0ec68844363240ef250efa014a77b762c2f01
SimeonNedyalkov <112097158+SimeonNedyalkov@users.noreply.github.com>
2024-08-30 12:13:32 +0300

    explore view made

--- react-native-shop/app/(tabs)/explore.tsx
+++ react-native-shop/app/(tabs)/explore.tsx
@@ -3,8 +3,8 @@ export default function HomeScreen() {
   return (
     <View style={styles.titleContainer}>
       <View style={{flex:2,flexDirection:'row',paddingTop:Platform.OS ==='android'?40:0,backgroundColor:'black',width:'100%'}}>
-        <View style={{backgroundColor:'orange',width:100,height:100}}></View>
-        <View style={{width:100,height:100,backgroundColor:'green'}}></View>
+        <View style={{backgroundColor:'orange',width:70,height:70,left:30,position:'absolute',top:Platform.OS ==='android'?40:0}}></View>
+        <View style={{width:70,height:70,backgroundColor:'green',right:30,position:'absolute',top:Platform.OS ==='android'?40:0}}></View>
       </View>
       
       <View style={{flex:8}}>
